9902064b0e4ae5b51bb65b3250deec954dd14eff
Cyr Mathieu GUEYE <mgueye@eurocybergroup.fr>
2024-11-28 15:42:13 +0100

    update: set up the main files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SwaggerModule } from '@nestjs/swagger';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    SwaggerModule,
+    NotificationsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
