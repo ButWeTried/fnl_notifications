96b0f72b1dd1ecd4cc534b062afc73f98681caec
Cyr Mathieu GUEYE <mgueye@eurocybergroup.fr>
2024-11-28 17:33:06 +0100

    update: setup of mongoDB

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  Notifications,
+  NotificationsSchema,
+} from 'src/schemas/notification.schema';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Notifications.name,
+        schema: NotificationsSchema,
+      },
+    ]),
+  ],
   controllers: [NotificationsController],
-  providers: [NotificationsService]
+  providers: [NotificationsService],
 })
 export class NotificationsModule {}
